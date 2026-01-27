633cc79b312aad7c21da40f053f313d1a5c146b0
273* <keita.y310@icloud.com>
2026-01-27 23:04:49 +0900

    add actions and theme

--- src/search-templates.tsx
+++ src/search-templates.tsx
@@ -1,6 +1,8 @@
-import { Grid } from "@raycast/api";
+import { Action, ActionPanel, environment, Grid, Icon } from "@raycast/api";
 import { BASE_URL } from "./core/constants";
 import { templates } from "./core/templates";
+
+const textColor = environment.appearance === "dark" ? "White" : "Black";
 
 export default function Command() {
   return (
@@ -8,7 +10,26 @@ export default function Command() {
       {Object.entries(templates).map(([category, equations]) => (
         <Grid.Section key={category} title={category}>
           {Object.entries(equations).map(([name, latex]) => (
-            <Grid.Item key={name} content={`${BASE_URL}?\\color{White}${latex}`} title={name} />
+            <Grid.Item
+              key={name}
+              content={`${BASE_URL}?\\color{${textColor}}${latex}`}
+              title={name}
+              actions={
+                <ActionPanel>
+                  <Action.CopyToClipboard title="Copy to Clipboard" content={latex} />
+                  <Action
+                    icon={Icon.PlusCircle}
+                    title="Create New Equation from Template"
+                    onAction={() => console.log("Create New Equation from Template")}
+                  />
+                  <Action
+                    icon={Icon.Download}
+                    title="Export Image"
+                    onAction={() => console.log("Export ImagDEFAULT_ICON")}
+                  />
+                </ActionPanel>
+              }
+            />
           ))}
         </Grid.Section>
       ))}
