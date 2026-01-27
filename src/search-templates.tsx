7dd3ad5193e9c82ada4bf257f6a4834a2857c419
273* <keita.y310@icloud.com>
2026-01-27 23:17:06 +0900

    add search filter

--- src/search-templates.tsx
+++ src/search-templates.tsx
@@ -1,13 +1,32 @@
 import { Action, ActionPanel, environment, Grid, Icon } from "@raycast/api";
 import { BASE_URL } from "./core/constants";
 import { templates } from "./core/templates";
+import { useState } from "react";
 
 const textColor = environment.appearance === "dark" ? "White" : "Black";
 
 export default function Command() {
+  const [selectedCategory, setSelectedCategory] = useState<string>("all");
+
+  const filteredTemplates = Object.entries(templates).filter(
+    ([cat]) => selectedCategory === "all" || cat === selectedCategory,
+  );
+
   return (
-    <Grid inset={Grid.Inset.Small} columns={6} searchBarPlaceholder="Search LaTeX Templates">
-      {Object.entries(templates).map(([category, equations]) => (
+    <Grid
+      inset={Grid.Inset.Small}
+      columns={6}
+      searchBarPlaceholder="Search LaTeX Templates"
+      searchBarAccessory={
+        <Grid.Dropdown tooltip="Equation Category" onChange={(v) => setSelectedCategory(v)} value={selectedCategory}>
+          <Grid.Dropdown.Item key="all" title="All Categories" value="all" />
+          {Object.keys(templates).map((cat) => (
+            <Grid.Dropdown.Item key={cat} title={cat} value={cat} />
+          ))}
+        </Grid.Dropdown>
+      }
+    >
+      {filteredTemplates.map(([category, equations]) => (
         <Grid.Section key={category} title={category}>
           {Object.entries(equations).map(([name, latex]) => (
             <Grid.Item
